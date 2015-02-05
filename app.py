8efb2d102024ddbc40c7cc9cc7b08fa7292c624a
lolhansoo <hansoolee85@gmail.com>
2015-02-05 09:30:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -99,10 +99,10 @@ def newaccount_post():
 	if request.method == 'POST':
 		user_name = request.form.get('username')
 		password = request.form.get('password')
-		if user_name.toString() == '':
+		if user_name == '':
 			flash("Please enter a username.", category='error')
 			return render_template('createaccount.html')
-		elif password.toString() == '':
+		elif password == '':
 			flash("Please enter a password.", category='error')
 			return render_template('createaccount.html')
 		users = db.users
