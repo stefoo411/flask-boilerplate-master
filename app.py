6f682e0464d4cee689951070491fa58ea41bf1e6
stefoo411 <snowflower411@yahoo.com>
2015-02-05 09:36:02 -0500

    login

--- app.py
+++ app.py
@@ -78,17 +78,16 @@ def post():
 		return render_template('post.html')
 	return render_template('get.html')
 
-#@app.route('/login', methods=['GET','POST'])
-#def login():
-#	users = db.users
-#	users = users.find({})
-#	user_name = request.form.get('username')
-#	password = request.form.get('password')
-#	for user in users:
-#		
-#
-#		if user =
-#	return render_template('get.html', users=users)
+@app.route('/login', methods=['GET','POST'])
+def login():
+	users = db.users
+	users = users.find({})
+	user_name = request.form.get('username')
+	pass_word = request.form.get('password')
+	for user in users:
+		if user == user_name && pass_word == user.password:
+			return render_template('home_login.html')
+	return render_template('get.html', users=users)
 
 @app.route('/newaccount')
 def newaccount():
