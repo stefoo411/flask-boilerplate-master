a1eeb8d6be941bfc6acc3c78da628728817efe7d
lolhansoo <hansoolee85@gmail.com>
2015-03-17 09:35:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ def newaccount_post():
 		elif password == '':
 			flash("Please enter a password.", category='error')
 			return render_template('createaccount.html')
-		users = db.users.count({users: user_name})
+		users = db.users.count({username: user_name})
 		if (user_exists == 1):
 			flash("That username already exists.", category='error')
 			return render_template('createaccount.html')
